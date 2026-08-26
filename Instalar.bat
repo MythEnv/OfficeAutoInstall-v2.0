4e4dcafce7952b4c9fc539eb30586f7b6751b242
wenliangk <wenliangk@gmail.com>
2026-08-25 21:18:47 -0500

    Delete Main

--- Instalar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-color 0b
-echo ==========================================
-echo    Preparando instalacion de Office...
-echo ==========================================
-powershell -ExecutionPolicy Bypass -File "%~dp0InstalarOffice.ps1"
